998b20978c8aa302b9f56641085f46457ee14a9e
netocalegari <netokalegari@gmail.com>
2025-01-22 10:28:12 -0300

    GET gama by id

--- src/main/java/com/neto/dslist/controllers/GameController.java
+++ src/main/java/com/neto/dslist/controllers/GameController.java
@@ -1,9 +1,11 @@
 package com.neto.dslist.controllers;
 
+import com.neto.dslist.dtos.GameDTO;
 import com.neto.dslist.dtos.GameMinDTO;
 import com.neto.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +20,10 @@ public class GameController {
     @GetMapping
     public List<GameMinDTO> findAll(){
         return gameService.findAll();
+    }
+
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id){
+        return gameService.findById(id);
     }
 }
--- src/main/java/com/neto/dslist/services/GameService.java
+++ src/main/java/com/neto/dslist/services/GameService.java
@@ -1,18 +1,31 @@
 package com.neto.dslist.services;
 
+import com.neto.dslist.dtos.GameDTO;
 import com.neto.dslist.dtos.GameMinDTO;
 import com.neto.dslist.entities.Game;
+import com.neto.dslist.exceptions.BadRequestException;
 import com.neto.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class GameService {
     @Autowired
     private GameRepository gameRepository;
+
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         return gameRepository.findAll().stream().map(GameMinDTO::new).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id) {
+        Game game = gameRepository.findById(id).orElseThrow(() -> new BadRequestException("Game not found"));
+
+        return new GameDTO(game);
     }
 }
