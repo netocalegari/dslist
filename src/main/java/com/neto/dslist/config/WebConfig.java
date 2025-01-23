739514484f9d38f0dcb45f5734f05ae505c0a7d2
netocalegari <netokalegari@gmail.com>
2025-01-23 09:40:03 -0300

    CORS config

--- src/main/java/com/neto/dslist/config/WebConfig.java
+++ src/main/java/com/neto/dslist/config/WebConfig.java
@@ -1,4 +1,23 @@
 package com.neto.dslist.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
 public class WebConfig {
+
+    @Value("${cors.origins}")
+    private String corsOrigins;
+
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedMethods("*").allowedOrigins(corsOrigins);
+            }
+        };
+    }
+
 }
